a228dcbc7ba7bd73bc06fdda310e71aaaac1b4a1
Charlie Birks <charlie@daft.games>
2023-02-04 18:12:47 +0000

    Remove PC assert

--- core/ARMv6MCore.cpp
+++ core/ARMv6MCore.cpp
@@ -1980,7 +1980,6 @@ void ARMv6MCore::updateTHUMBPC(uint32_t pc)
 {
     // called when PC is updated in THUMB mode (except for incrementing)
     assert(!(pc & 1));
-    assert(pc < 0x40000000);
 
     if(pcPtr && pc >> 24 == loReg(Reg::PC) >> 24)
     {
